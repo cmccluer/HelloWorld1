561415624390697f4a96f81302e6e41199eb0e10
Chris Mccluer <cmccluer@labconco.com>
2019-03-08 09:52:34 -0600

    Initial commit HelloWorld1 03/08/2019

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName = "";
+            int userAge = 0;
+            int currentYear = 0;
+
+            Console.Write("Enter name: ");
+            userName = Console.ReadLine();
+            Console.Write("Enter age ");
+            userAge = Convert.ToInt32(Console.ReadLine());
+            Console.Write("enter current year: ");
+            currentYear = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Hello World! My Name is {0} and I am {1} years old. I was born in {2}.", userName, userAge, currentYear - userAge);
+            Console.Read();
+        }
+    }
+}
